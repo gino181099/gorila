c9c930aa00c12aaf9662b7c47cb624d9ff884fc7
Gino Siculo <ginofranco2010@live.com>
2022-07-04 11:03:27 -0300

    buscador andando

--- components/reserve.js
+++ components/reserve.js
@@ -3,6 +3,24 @@ import { useState } from "react";
 const Reserve = () => {
   const [checkIn, setCheckIn] = useState("2022-07-03");
   const [checkOut, setCheckOut] = useState("2022-07-03");
+  const onChangeCheckIn = (e) => {
+    const newDate = new Date(e.target.value);
+    let curr_date = (newDate.getDate() + 1).toString().padStart(2, "0");
+    let curr_month = (newDate.getMonth() + 1).toString().padStart(2, "0");
+    let curr_year = newDate.getFullYear();
+    const dateToPass = `${curr_year}-${curr_month}-${curr_date}`;
+    console.log(dateToPass);
+    setCheckIn(dateToPass);
+  };
+  const onChangeCheckOut = (e) => {
+    const newDate = new Date(e.target.value);
+    let curr_date = (newDate.getDate() + 1).toString().padStart(2, "0");
+    let curr_month = (newDate.getMonth() + 1).toString().padStart(2, "0");
+    let curr_year = newDate.getFullYear();
+    const dateToPass = `${curr_year}-${curr_month}-${curr_date}`;
+    console.log(dateToPass);
+    setCheckOut(dateToPass);
+  };
   return (
     <div className="reserve">
       <p className="title__reserve">Reserva Ahora!</p>
@@ -20,6 +38,7 @@ const Reserve = () => {
           name="start_date_mobile"
           className="date_mobile widget_hide"
           id="start_date_mobile"
+          onChange={onChangeCheckIn}
         ></input>
         <label
           className="control-label"
@@ -33,11 +52,12 @@ const Reserve = () => {
           name="end_date_mobile"
           className="date_mobile widget_hide"
           id="end_date_mobile"
+          onChange={onChangeCheckOut}
         ></input>
         <a
           className="submit_link"
           target="_blank"
-          href="https://hotels.cloudbeds.com/es/reservation/vrb2dl?ga_sess_id=1641116198.1656893645#checkin=2022-07-03&amp;checkout=2022-07-04"
+          href={`https://hotels.cloudbeds.com/es/reservation/vrb2dl?ga_sess_id=1641116198.1656893645#checkin=${checkIn}&amp;checkout=${checkOut}`}
         >
           Ir
         </a>
